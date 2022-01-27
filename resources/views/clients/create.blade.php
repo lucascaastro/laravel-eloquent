383b0fb0ef59498ad652da3a5c2839f7a2f3b7b4
lucascaastro <lucasgrrock@gmail.com>
2022-01-27 20:39:15 -0300

    first commit

---
+++ resources/views/clients/create.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<form action="/clients/store" method="POST">
+    @csrf
+    <input type="text" name="name" placeholder="Nome">
+    <input type="text" name="email" placeholder="Email">
+    <input type="text" name="phone" placeholder="Telefone">
+    <input type="text" name="id_number" placeholder="CPF">
+    <button type="submit">Enviar</button>
+</form>
+
+<body>
+
+</body>
+
+</html>
