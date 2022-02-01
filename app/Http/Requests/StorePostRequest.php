aa9de96df7c7a5a75e83d07d1ff399b898d544f2
lucascaastro <lucasgrrock@gmail.com>
2022-01-31 21:20:58 -0300

    rules

--- app/Http/Requests/StorePostRequest.php
+++ app/Http/Requests/StorePostRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoreClientRequest extends FormRequest
+class StorePostRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -13,7 +13,7 @@ class StoreClientRequest extends FormRequest
      */
     public function authorize()
     {
-        return true;
+        return false;
     }
 
     /**
@@ -24,13 +24,7 @@ class StoreClientRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required',
-        ];
-    }
-    public function messages()
-    {
-        return [
-            'name.required' => 'Um nome é obrigatório',
+            //
         ];
     }
 }
