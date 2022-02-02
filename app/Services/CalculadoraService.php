06a53e45c4a123be4ce873f9975fc6d3acb0f687
lucascaastro <lucasgrrock@gmail.com>
2022-02-01 21:42:36 -0300

    Service Soma

---
+++ app/Services/CalculadoraService.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Post;
+use App\Repositories\PostRepository;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+
+class PostService
+{
+    /**
+     * @var PostRepository
+     */
+
+    public function sum($num1, $num2)
+    {
+
+        // DB::beginTransaction();
+        try {
+            $result = $num1 + $num2;
+        } catch (\Throwable $th) {
+            // DB::rollBack();
+            logger()->error($th);
+            return [
+                'success' => false,
+                'message' => 'Erro ao fazer soma'
+            ];
+        }
+        // DB::commit();
+        return [
+            'success' => true,
+            'message' => 'Soma feita com sucesso',
+            'data' => $result
+        ];
+    }
+}
