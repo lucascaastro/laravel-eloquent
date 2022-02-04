771e9779786540af0d6d13d8e737e1432cf7e413
lucascaastro <lucasgrrock@gmail.com>
2022-02-04 00:04:01 -0300

    Factory Bill

--- database/factories/BillFactory.php
+++ database/factories/BillFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class BillFactory extends Factory
@@ -14,7 +15,12 @@ class BillFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'invoice' => $this->faker->randomDigit('número de 4 dígitos aleatório'),
+            'installment' => $this->faker->randomDigit('número de 1 dígito aleatório'),
+            'client_id' => User::factory()->create->id,
+            'value' => $this->faker->randomDigit('número de telefone falso'),
+            'due_date' =>  $this->faker->name('data entre now e +1 week'),
+            'payment_date' =>  $this->faker->name('data entre -1 week e now'),
         ];
     }
 }
